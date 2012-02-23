76c114b5a90777b4aa0a8be3468e697f3d479215
Javier Velazquez <javier.velazquez@crowdint.com>
2012-02-23 16:50:15 -0600

    added index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Sports::Application.routes.draw do
 
   resources :actividades
 
-  root :to => 'actividades#index'
+  resources :index
+
+  root :to => 'index#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
