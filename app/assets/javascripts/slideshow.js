1972d1e187c4776ac8924ba62c06141c998ad0be
Javier Velazquez <javier.velazquez@crowdint.com>
2012-03-22 17:22:20 -0600

    index view with slideshow

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,69 +1,62 @@
-$slideshow = {
-    context: false,
-    tabs: false,
-    timeout: 1000,      // time before next slide appears (in ms)
-    slideSpeed: 1000,   // time it takes to slide in each slide (in ms)
-    tabSpeed: 300,      // time it takes to slide in each slide (in ms) when clicking through tabs
-    fx: 'scrollLeft',   // the slide effect to use
+$(document).ready(function() { 
+    var currentPosition = 0; 
+    var slideWidth = 900; 
+    var slides = $('.slide'); 
+    var numberOfSlides = slides.length; 
+    var slideShowInterval; 
+    var speed = 3000; 
     
-    init: function() {
-        // set the context to help speed up selectors/improve performance
-        this.context = $('#slideshow');
-        
-        // set tabs to current hard coded navigation items
-        this.tabs = $('ul.slides-nav li', this.context);
-        
-        // remove hard coded navigation items from DOM 
-        // because they aren't hooked up to jQuery cycle
-        this.tabs.remove();
-        
-        // prepare slideshow and jQuery cycle tabs
-        this.prepareSlideshow();
-    },
-    
-    prepareSlideshow: function() {
-        // initialise the jquery cycle plugin -
-        // for information on the options set below go to: 
-        // http://malsup.com/jquery/cycle/options.html
-        $('div.slides > ul', $slideshow.context).cycle({
-            fx: $slideshow.fx,
-            timeout: $slideshow.timeout,
-            speed: $slideshow.slideSpeed,
-            fastOnEvent: $slideshow.tabSpeed,
-            pager: $('ul.slides-nav', $slideshow.context),
-            pagerAnchorBuilder: $slideshow.prepareTabs,
-            before: $slideshow.activateTab,
-            pauseOnPagerHover: true,
-            pause: true
-        });            
-    },
-    
-    prepareTabs: function(i, slide) {
-        // return markup from hardcoded tabs for use as jQuery cycle tabs
-        // (attaches necessary jQuery cycle events to tabs)
-        return $slideshow.tabs.eq(i);
-    },
-
-    activateTab: function(currentSlide, nextSlide) {
-        // get the active tab
-        var activeTab = $('a[href="#' + nextSlide.id + '"]', $slideshow.context);
-        
-        // if there is an active tab
-        if(activeTab.length) {
-            // remove active styling from all other tabs
-            $slideshow.tabs.removeClass('on');
-            
-            // add active styling to active button
-            activeTab.parent().addClass('on');
-        }            
-    }            
-};
+    slideShowInterval = setInterval(changePosition, speed); 
+    slides.wrapAll('<div id="slidesHolder"></div>') 
+    slides.css({ 'float' : 'left' }); 
 
 
-$(function() {
-    // add a 'js' class to the body
-    $('body').addClass('js');
-    
-    // initialise the slideshow when the DOM is ready
-    $slideshow.init();
-});  
+    $('#slideshow') 
+        .prepend('<span class="nav" id="leftNav">Move Left</span>') 
+        .append('<span class="nav" id="rightNav">Move Right</span>');
+
+
+    $('#slidesHolder').css('width', slideWidth * numberOfSlides); 
+    function changePosition() { 
+        if(currentPosition == numberOfSlides - 1) { 
+            currentPosition = 0; 
+            manageNav(currentPosition);
+        } 
+        else { 
+        currentPosition++;
+        manageNav(currentPosition); 
+        } 
+        moveSlide(); 
+    } 
+
+    function moveSlide() { 
+        $('#slidesHolder') 
+            .animate({'marginLeft' : slideWidth*(-currentPosition)}); 
+        } 
+
+
+    function manageControls(position) {  
+
+        if(position==0){ $('#leftNav').hide() } 
+            else { $('#leftNav').show() } 
+         if(position==numberOfSlides-1){ $('#rightNav').hide() } else { $('#rightNav').show() } }
+
+    manageNav(currentPosition);
+
+    function manageNav(position) {  if(position==0){ $('#leftNav').hide() } 
+
+        else { $('#leftNav').show() } 
+         if(position==numberOfSlides-1){ $('#rightNav').hide() } 
+        else { $('#rightNav').show() } }
+
+
+    $('.nav').bind('click', function() { 
+        currentPosition = ($(this).attr('id')=='rightNav') ? currentPosition+1 : currentPosition-1; 
+         manageNav(currentPosition);
+        clearInterval(slideShowInterval); 
+        slideShowInterval = setInterval(changePosition, speed); 
+        moveSlide(); 
+
+    }); 
+
+});
