f01f4500beb15935ee4384884f75f26d53876fd0
javier velazquez <javier.velazquez@crowdint.com>
2012-06-04 17:47:15 -0500

    modified views and to order menus

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -9,31 +9,31 @@ var slideshowSpeed = 6000;
 // which also includes some text and url's.
 var photos = [ {
 		"title" : "Stairs",
-		"image" : "vacation.jpg",
+		"image" : "loras.jpg",
 		"url" : "http://www.sxc.hu/photo/1271909",
 		"firstline" : "Going on",
 		"secondline" : "vacation?"
 	}, {
 		"title" : "Office Appartments",
-		"image" : "work.jpg",
+		"image" : "rondalla.jpg",
 		"url" : "http://www.sxc.hu/photo/1265695",
 		"firstline" : "Or still busy at",
 		"secondline" : "work?"
 	}, {
 		"title" : "Mountainbiking",
-		"image" : "biking.jpg",
+		"image" : "loritas.jpg",
 		"url" : "http://www.sxc.hu/photo/1221065",
 		"firstline" : "Get out and be",
 		"secondline" : "active"
 	}, {
 		"title" : "Mountains Landscape",
-		"image" : "nature.jpg",
+		"image" : "loros2.jpg",
 		"url" : "http://www.sxc.hu/photo/1271915",
 		"firstline" : "Take a fresh breath of",
 		"secondline" : "nature"
 	}, {
 		"title" : "Italian pizza",
-		"image" : "food.jpg",
+		"image" : "fime.jpg",
 		"url" : "http://www.sxc.hu/photo/1042413",
 		"firstline" : "Enjoy some delicious",
 		"secondline" : "food"
